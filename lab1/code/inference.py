da4d3b81d089f9dc7ababfb885ec89b2f0aa191b
Aris Tsakpinis <tsaris@amazon.de>
2023-04-14 01:00:09 +0200

    update lab1, make it region agnostic and smaller tweaks

--- lab1/code/inference.py
+++ lab1/code/inference.py
@@ -5,9 +5,9 @@ from transformers import AutoTokenizer, GPTJForCausalLM, pipeline
 
 def model_fn(model_dir):
 
-    tokenizer = AutoTokenizer.from_pretrained("EleutherAI/gpt-j-6B")
+    tokenizer = AutoTokenizer.from_pretrained("EleutherAI/gpt-j-6b")
     
-    model = GPTJForCausalLM.from_pretrained("EleutherAI/gpt-j-6B", revision="float16", torch_dtype=torch.float16)
+    model = GPTJForCausalLM.from_pretrained("EleutherAI/gpt-j-6b", revision="float16", torch_dtype=torch.float16)
 
     print('Model downloaded and loaded into memory...')
 
